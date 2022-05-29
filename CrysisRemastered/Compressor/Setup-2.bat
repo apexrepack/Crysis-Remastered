ca7f91b3e6df96f4f58c4bdb699c4aa173ba81c7
APEX Repack <106033868+apexrepack@users.noreply.github.com>
2022-05-29 19:45:39 +0600

    Add files via upload

---
+++ CrysisRemastered/Compressor/Setup-2.bat
@@ -0,0 +1,21 @@
+@echo off
+color 0a
+Title Crysis Remastered Compressor By Developer Edition
+echo *******************************************************************************************
+echo *                 Crysis Remastered Data Compressor By Developer Edition                  *
+echo *     Don't Any .bat Files Open Or Edit Because All .bat Files Are Compressor Program     *
+echo *******************************************************************************************
+echo *                         Time:%time%     Date:%date%                        *
+echo *******************************************************************************************
+@echo Compressor Developer By APEX Repack
+pause
+@echo Working Program FreeArc 0.67, Srep64, Lzma2 And Precomp 0.43 Only
+arc a -lc8 -ep1 -ed -r -w.\ E:\CrysisRemastered\Output\Setup-2.bin -msrep+lzma:a1:mfbt4:d512m:fb256:mc1000:lc8 "E:\CrysisRemastered\GameFiles\Data2\*"
+cls
+echo *******************************************************************************************
+echo.
+echo                                 Compressing is Completed
+echo.
+echo *******************************************************************************************
+@echo Compressor Developer By APEX Repack
+pause
